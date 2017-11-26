88161d44e2af5d7cabf4df5349b44ddee8619d6d
Acaledus <33514818+Acaledus@users.noreply.github.com>
2017-11-26 14:06:48 +0100

    Aufgaben 1 und 2 fertig.
    Und diesmal funktioniert auch alles.

--- PSE_2017_4_Slot_23_CapkanScheichWalcher_project/src/de/unistuttgart/iaas/pse/ex04/p1/IOConsole.java
+++ PSE_2017_4_Slot_23_CapkanScheichWalcher_project/src/de/unistuttgart/iaas/pse/ex04/p1/IOConsole.java
@@ -1,5 +1,7 @@
 package de.unistuttgart.iaas.pse.ex04.p1;
 
+import java.util.Scanner;
+
 /**
  * Klasse zur Berechnung von Flächen.
  * @author Daniel Capkan, Matrikelnummer: 3325960, st156303@stud.uni-stuttgart.de
@@ -90,8 +92,40 @@ public class IOConsole {
 		return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)) + a + b; //Hier ist keine Exception nötig, da a und b an dieser Stelle gar nicht mehr negativ sein können.
 	}
 
+	/**
+	 * Ein Menü, das dem Benutzer erlaubt, Flächeninhalt und Umfang von drei verschiedenen Grundformen zu berechnen.
+	 */
 	public static void printMenu() {
-		//Noch zu erledigen
+		int a, b, r;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Wähle die Fläche: \n1: Kreis\n2: rechtwinkliges Dreieck\n3: Rechteck");
+		switch(sc.nextInt()) {
+		case 1:
+			System.out.println("Welchen Radius soll der Kreis haben?");
+			r = sc.nextInt();
+			System.out.println("Flächeninhalt: " + computeCircleArea(r));
+			System.out.println("Umfang: " + computeCircleCircumference(r));
+			break;
+		case 2:
+			System.out.println("Was soll die erste Kantenlänge sein?");
+			a = sc.nextInt();
+			System.out.println("Was soll die zweite kantenlänge sein?");
+			b = sc.nextInt();
+			System.out.println("Flächeninhalt: " + computeTriangleArea(a, b));
+			System.out.println("Umfang: " + computeTriangleCircumference(a, b));
+			break;
+		case 3:
+			System.out.println("Was soll die erste Kantenlänge sein?");
+			a = sc.nextInt();
+			System.out.println("Was soll die zweite kantenlänge sein?");
+			b = sc.nextInt();
+			System.out.println("Flächeninhalt: " + computeRectangleArea(a, b));
+			System.out.println("Umfang: " + computeRectangleCircumference(a, b));
+			break;
+		default:
+			System.out.println("Ungültige Eingabe.");
+		}
+		sc.close();
 	}
 
 	public static void main(String[] args) {
--- PSE_2017_4_Slot_23_CapkanScheichWalcher_project/src/de/unistuttgart/iaas/pse/ex04/p2/StringFunctions.java
+++ PSE_2017_4_Slot_23_CapkanScheichWalcher_project/src/de/unistuttgart/iaas/pse/ex04/p2/StringFunctions.java
@@ -1,17 +1,64 @@
 package de.unistuttgart.iaas.pse.ex04.p2;
 
+import java.util.Scanner;
+
+/**
+ * Eine Klasse mit verschiedenen Methoden zur Untersuchung von Strings.
+ * @author Daniel Capkan, Matrikelnummer: 3325960, st156303@stud.uni-stuttgart.de
+ * @author Mario Scheich, Matrikelnummer: 3232655 , st151491@stud.uni-stuttgart.de
+ * @author Florian Walcher, Matrikelnummer: 3320185, st156818@stud.uni-stuttgart.de
+ */
 public class StringFunctions {
-
+	/**
+	 * Eine Methode, die die Anzahl der Diphthonge in einem String z‰hlt.
+	 * @param testString - der zu untersuchende String.
+	 * @return - die Anzahl der Diphthonge.
+	 */
 	public static int numberOfDiphthongs(String testString) {
-		return 0;
+		testString = testString.toLowerCase(); //Damit Diphthonge mit Groﬂbuchstaben, zum Beispiel bei "Auenland" auch erkannt werden.
+		String[] buchstabenpaar = new String[testString.length() - 1];
+		for(int i = 0; i < testString.length() - 1; i++) {
+			buchstabenpaar[i] = testString.substring(i, i + 2);
+		}
+		int anzahl = 0;
+		for (String s : buchstabenpaar) {
+			if(s.equals("ai")||s.equals("eu")||s.equals("au")||s.equals("‰u")||s.equals("ei")||s.equals("ui")) {
+				anzahl++;
+			}
+		}
+		return anzahl;
 	}
-
+	
+	/**
+	 * ‹berpr¸ft, ob ein String ein Palindrom ist.
+	 * @param testString - der zu ¸berpr¸fende String.
+	 * @return true, wenn der String ein Palindrom ist und false, wenn er es nicht ist.
+	 */
 	public static boolean isPalindrome(String testString) {
-		return false;
+		testString = testString.toLowerCase(); //Damit groﬂ geschriebene Wˆrter wie "Rentner" als Palindrome erkannt werden.
+		return testString.contains(new StringBuilder(testString).reverse().toString());
 	}
 
+	/**
+	 * Ein Spiel, bei dem der Spieler zwei Mal einen String eingibt. 
+	 * Wenn der erste ein Palindrom ist und der zweite
+	 * mehr als drei Diphthonge enth‰lt, hat der Spieler gewonnen. 
+	 * @param args - nicht genutzt.
+	 */
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Gib einen String ein. Wenn er ein Palindrom ist, kommst du weiter.");
+		if (isPalindrome(sc.next())) {
+			System.out.println("Gib einen String ein. Wenn er mehr als drei Diphthonge enth‰lt, hast du gewonnen.");
+			if (numberOfDiphthongs(sc.next()) > 3) {
+				System.out.println("Du hast gewonnen!");
+			} else {
+				System.out.println("Leider zu wenige Diphthonge.");
+			}
+		} else {
+			System.out.println("Das ist kein Palindrom.");
+		}
+		sc.close();
 	}
 
 }
